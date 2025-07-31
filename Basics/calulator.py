7e192ab6bbbf5c7f9b6a5ed48a39894aab13c96f
Heet Mehta <heetmehta18125@gmail.com>
2025-07-31 18:57:27 +0530

    Initial commit

---
+++ Basics/calulator.py
@@ -0,0 +1,14 @@
+print("Welcome to the calulator program");
+
+a= float(input("Enter first number: "))
+b= float(input("Enter second number: "))
+
+print("The sum of a and b is: ", a + b)
+print("The difference of a and b is: ", a - b)
+print("The product of a and b is: ", a * b)
+print("The quotient of a and b is: ", a / b)    
+print("The modulus of a and b is: ", a % b)
+print("The exponent of a to the power of b is: ", a ** b)
+
+
+input("Press Enter to exit...")  # Keeps the console open until user presses Enter
